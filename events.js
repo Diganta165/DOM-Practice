e094b12ad5a09a13c5fbdbb13391731d31aa55db
Diganta165 <mdsrdiganta@gmail.com>
2021-08-13 04:34:13 +0600

    trying different ways of onclick handler

---
+++ events.js
@@ -0,0 +1,11 @@
+function makeMeRed(){
+    document.body.style.backgroundColor = 'red';
+}
+
+
+    const blue = document.getElementById('Make-Me-Blue');
+    blue.onclick = makeBlue;
+
+    function makeBlue(){
+        document.body.style.backgroundColor = "blue";
+    }
