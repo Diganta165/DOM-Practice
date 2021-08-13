a97f075cf075b3a643ce038d4b5c56140f227550
Diganta165 <mdsrdiganta@gmail.com>
2021-08-13 20:12:25 +0600

    Different ways of using event listener

--- events.js
+++ events.js
@@ -1,11 +1,41 @@
-function makeMeRed(){
+function makeMeRed() {
     document.body.style.backgroundColor = 'red';
 }
+// **************
+
+const blue = document.getElementById('Make-Me-Blue');
+blue.onclick = makeBlue;
+
+function makeBlue() {
+    document.body.style.backgroundColor = "blue";
+}
+// *************************
 
 
-    const blue = document.getElementById('Make-Me-Blue');
-    blue.onclick = makeBlue;
+const green = document.getElementById('Make-Me-Green');
+green.onclick = function (){
+    document.body.style.backgroundColor = 'green';
+} 
+// *****************************
 
-    function makeBlue(){
-        document.body.style.backgroundColor = "blue";
-    }
+const goldenRod = document.getElementById('Make-Me-GoldenRod')
+
+goldenRod.addEventListener('click',makeGoldenRod);
+
+function makeGoldenRod(){
+    document.body.style.backgroundColor = 'goldenrod'
+}
+
+// *********
+
+const hotPink = document.getElementById('Make-Me-HotPink');
+
+hotPink.addEventListener('click', function(){
+    document.body.style.backgroundColor = 'hotpink';
+})
+
+// ************
+
+document.getElementById('Make-Me-LightBlue').addEventListener('click', function(){
+    document.body.style.backgroundColor = 'lightblue';
+})
